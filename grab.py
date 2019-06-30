bdd4cff89a363a31dde0619b70cbe7b81d09f315
Alexander Larin <ekzebox@gmail.com>
2019-06-30 15:07:32 +0300

    Remove cut_size selection for frame motion area, add blob_size face detector param and file log config

--- grab.py
+++ grab.py
@@ -14,8 +14,8 @@ def grab_loop(config, stream, channel, exchange):
     width, height = config['stream']['size']
     logger.info('stream size: {size}'.format(size=(width, height)))
 
-    detector_sizes = [(int(width * sw), int(height * sh)) for sw, sh in config['stream']['cuts']]
-    logger.info('detector sizes: {detector_sizes}'.format(detector_sizes=detector_sizes))
+    blob_size = tuple(config['face_detector']['blob_size'])
+    logger.info('min grab size: {blob_size}'.format(blob_size=blob_size))
 
     logger.info('check init frame from stream')
     image = stream.read()
@@ -40,7 +40,8 @@ def grab_loop(config, stream, channel, exchange):
         if len(rects):
             # TODO: use biggest rect, not a first
             # TODO: detector sizes is redundant feature after face detector became single
-            motion_frame = frame.fit_cut(*rects[0], detector_sizes)
+            # TODO: fit_cut should be greater than blob size but less than entire image size
+            motion_frame = frame.cut(*rects[0])
             logger.info('publish frame {bounds}'.format(bounds=motion_frame.bounds))
             producer.publish(motion_frame.to_json(),
                              exchange=exchange,
